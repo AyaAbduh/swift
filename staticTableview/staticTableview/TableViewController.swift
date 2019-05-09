fd77d0f2d5a264eb69f93cfea0e6ed61f2670a9d
YoyaMohamed <aya.yoya962015@gmail.com>
2019-05-09 14:49:00 +0200

    static table

---
+++ staticTableview/staticTableview/TableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewController.swift
+//  staticTableview
+//
+//  Created by Esraa Hassan on 5/9/19.
+//  Copyright © 2019 Esraa And Passant. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
